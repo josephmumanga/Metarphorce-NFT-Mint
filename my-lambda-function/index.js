795f673c4b8c9ead80303a617d2ecfedcfa1f8bc
josephmumanga <138316076+josephmumanga@users.noreply.github.com>
2023-07-03 21:42:55 +0000

    aws configurations added

---
+++ my-lambda-function/index.js
@@ -0,0 +1,28 @@
+const AWS = require('aws-sdk');
+
+exports.handler = async (event, context) => {
+  const ssm = new AWS.SSM();
+
+  try {
+    // Execute the commands on the EC2 instance using Systems Manager Run Command
+    await ssm.sendCommand({
+      DocumentName: 'AWS-RunShellScript',
+      InstanceIds: ['i-1234567890'], // Replace with your EC2 instance ID
+      Parameters: {
+        commands: [
+          'nvm use --lts',
+          'truffle migrate --reset --network mumbai',
+          'truffle exec scripts/mintNFT.js --network mumbai'
+        ]
+      }
+    }).promise();
+
+    return {
+      statusCode: 200,
+      body: 'Commands executed successfully'
+    };
+  } catch (error) {
+    console.error('Error executing commands:', error);
+    throw error;
+  }
+};
